efbab81403ee2452e56ec2db3f4fc2541abfce43
Alberto De Bortoli <albertodebortoli@gmail.com>
2018-11-21 16:39:30 +0000

    Show Tweak description in TweaksConfigurationViewController

--- JustTweak/Classes/UI/BooleanTweakTableViewCell.swift
+++ JustTweak/Classes/UI/BooleanTweakTableViewCell.swift
@@ -7,6 +7,14 @@ import UIKit
 
 internal class BooleanTweakTableViewCell: UITableViewCell, TweaksConfigurationViewControllerCell {
     
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     var title: String? {
         get {
             return textLabel?.text
@@ -15,6 +23,16 @@ internal class BooleanTweakTableViewCell: UITableViewCell, TweaksConfigurationVi
             textLabel?.text = newValue
         }
     }
+    
+    var desc: String? {
+        get {
+            return detailTextLabel?.text
+        }
+        set {
+            detailTextLabel?.text = newValue
+        }
+    }
+    
     var value: TweakValue {
         get {
             return switchControl.isOn
