b86631917203c516bce0d7cab31ac27ceef5714a
Mathew Polzin <mpolzin@workwithopal.com>
2019-01-10 07:45:31 -0800

    Add an Xcode project with a shared scheme that builds the iOS target for Carthage support.

---
+++ JustTweak/JustTweak.h
@@ -0,0 +1,18 @@
+//
+//  JustTweak.h
+//  JustTweak
+//
+//  Created by Mathew Polzin on 1/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustTweak.
+FOUNDATION_EXPORT double JustTweakVersionNumber;
+
+//! Project version string for JustTweak.
+FOUNDATION_EXPORT const unsigned char JustTweakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustTweak/PublicHeader.h>
+
+
