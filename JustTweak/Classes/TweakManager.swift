8f7a0d85d4d9b8cb8cfe13998eeae158faf82d8c
Alberto De Bortoli <albertodebortoli@gmail.com>
2019-12-18 11:46:50 +0000

    Remove serial queue from TweakManager. Mark TweakManager's internal prolperties as Atomic.

--- JustTweak/Classes/TweakManager.swift
+++ JustTweak/Classes/TweakManager.swift
@@ -25,12 +25,10 @@ final public class TweakManager {
         }
     }
     
-    private let queue = DispatchQueue(label: "com.justeat.tweakManager")
-    
-    private var featureCache = [String : Bool]()
-    private var tweakCache = [String : [String : Tweak]]()
-    private var experimentCache = [String : String]()
-    private var observersMap = [NSObject : NSObjectProtocol]()
+    @Atomic private var featureCache = [String : Bool]()
+    @Atomic private var tweakCache = [String : [String : Tweak]]()
+    @Atomic private var experimentCache = [String : String]()
+    @Atomic private var observersMap = [NSObject : NSObjectProtocol]()
     
     var mutableConfiguration: MutableConfiguration? {
         return configurations.first { $0 is MutableConfiguration } as? MutableConfiguration
@@ -53,103 +51,96 @@ final public class TweakManager {
 extension TweakManager: MutableConfiguration {
     
     public func isFeatureEnabled(_ feature: String) -> Bool {
-        queue.sync {
-            if useCache, let cachedFeature = featureCache[feature] {
-                logClosure?("Feature '\(cachedFeature)' found in cache.)", .verbose)
-                return cachedFeature
-            }
-            
-            var enabled = false
-            for (_, configuration) in configurations.enumerated() {
-                if configuration.isFeatureEnabled(feature) {
-                    enabled = true
-                    break
-                }
-            }
-            if useCache {
-                featureCache[feature] = enabled
-            }
-            return enabled
+        if useCache, let cachedFeature = featureCache[feature] {
+            logClosure?("Feature '\(cachedFeature)' found in cache.)", .verbose)
+            return cachedFeature
         }
+        
+        var enabled = false
+        for (_, configuration) in configurations.enumerated() {
+            if configuration.isFeatureEnabled(feature) {
+                enabled = true
+                break
+            }
+        }
+        if useCache {
+            _featureCache.mutate { $0[feature] = enabled }
+        }
+        return enabled
     }
     
     public func tweakWith(feature: String, variable: String) -> Tweak? {
-        queue.sync {
-            if useCache, let cachedTweaks = tweakCache[feature], let cachedTweak = cachedTweaks[variable] {
-                logClosure?("Tweak '\(cachedTweak)' found in cache.)", .verbose)
-                return cachedTweak
-            }
-            
-            var result: Tweak? = nil
-            for (_, configuration) in configurations.enumerated() {
-                if let tweak = configuration.tweakWith(feature: feature, variable: variable) {
-                    logClosure?("Tweak '\(tweak)' found in configuration \(configuration))", .verbose)
-                    result = Tweak(feature: feature,
-                                   variable: variable,
-                                   value: tweak.value,
-                                   title: tweak.title,
-                                   group: tweak.group,
-                                   source: "\(type(of: configuration))")
-                    break
-                }
-                else {
-                    logClosure?("Tweak with identifier '\(variable)' NOT found in configuration \(configuration))", .verbose)
-                }
-            }
-            if let result = result {
-                logClosure?("Tweak with feature '\(feature)' and variable '\(variable)' resolved. Using '\(result)'.", .debug)
-                if useCache {
-                    if let _ = tweakCache[feature] {
-                        tweakCache[feature]?[variable] = result
-                    } else {
-                        tweakCache[feature] = [variable : result]
-                    }
-                }
+        if useCache,
+            let cachedTweaks = tweakCache[feature],
+            let cachedTweak = cachedTweaks[variable] {
+            logClosure?("Tweak '\(cachedTweak)' found in cache.)", .verbose)
+            return cachedTweak
+        }
+        
+        var result: Tweak? = nil
+        for (_, configuration) in configurations.enumerated() {
+            if let tweak = configuration.tweakWith(feature: feature, variable: variable) {
+                logClosure?("Tweak '\(tweak)' found in configuration \(configuration))", .verbose)
+                result = Tweak(feature: feature,
+                               variable: variable,
+                               value: tweak.value,
+                               title: tweak.title,
+                               group: tweak.group,
+                               source: "\(type(of: configuration))")
+                break
             }
             else {
-                logClosure?("No Tweak found for identifier '\(variable)'", .verbose)
+                logClosure?("Tweak with identifier '\(variable)' NOT found in configuration \(configuration))", .verbose)
             }
-            return result
         }
+        if let result = result {
+            logClosure?("Tweak with feature '\(feature)' and variable '\(variable)' resolved. Using '\(result)'.", .debug)
+            if useCache {
+                if let _ = tweakCache[feature] {
+                    _tweakCache.mutate { $0[feature]?[variable] = result }
+                } else {
+                    _tweakCache.mutate { $0[feature] = [variable : result] }
+                }
+            }
+        }
+        else {
+            logClosure?("No Tweak found for identifier '\(variable)'", .verbose)
+        }
+        return result
     }
     
     public func activeVariation(for experiment: String) -> String? {
-        queue.sync {
-            if useCache, let cachedExperiment = experimentCache[experiment] {
-                logClosure?("Experiment '\(cachedExperiment)' found in cache.)", .verbose)
-                return cachedExperiment
-            }
-            
-            var activeVariation: String?
-            for (_, configuration) in configurations.enumerated() {
-                activeVariation = configuration.activeVariation(for: experiment)
-                if activeVariation != nil { break }
-            }
-            if useCache {
-                experimentCache[experiment] = activeVariation
-            }
-            return activeVariation
+        if useCache, let cachedExperiment = experimentCache[experiment] {
+            logClosure?("Experiment '\(cachedExperiment)' found in cache.)", .verbose)
+            return cachedExperiment
         }
+        
+        var activeVariation: String? = nil
+        for (_, configuration) in configurations.enumerated() {
+            activeVariation = configuration.activeVariation(for: experiment)
+            if activeVariation != nil { break }
+        }
+        if useCache {
+            _experimentCache.mutate { $0[experiment] = activeVariation }
+            experimentCache[experiment] = activeVariation
+        }
+        return activeVariation
     }
     
     public func set(_ value: TweakValue, feature: String, variable: String) {
         guard let mutableConfiguration = self.mutableConfiguration else { return }
         if useCache {
-            queue.sync {
-                // cannot use write-through cache because tweakWith(feature:variable:) returns a Tweak, but here we only have a TweakValue
-                // we simply set the entry to nil so the next fetch will go through the list of configurations and subsequently re-cache
-                tweakCache[feature]?[variable] = nil
-            }
+            // cannot use write-through cache because tweakWith(feature:variable:) returns a Tweak, but here we only have a TweakValue
+            // we simply set the entry to nil so the next fetch will go through the list of configurations and subsequently re-cache
+            _tweakCache.mutate { $0[feature]?[variable] = nil }
         }
         mutableConfiguration.set(value, feature: feature, variable: variable)
     }
-
+    
     public func deleteValue(feature: String, variable: String) {
         guard let mutableConfiguration = self.mutableConfiguration else { return }
         if useCache {
-            queue.sync {
-                tweakCache[feature]?[variable] = nil
-            }
+            _tweakCache.mutate { $0[feature]?[variable] = nil }
         }
         mutableConfiguration.deleteValue(feature: feature, variable: variable)
     }
@@ -158,25 +149,21 @@ extension TweakManager: MutableConfiguration {
 extension TweakManager {
     
     public func registerForConfigurationsUpdates(_ object: NSObject, closure: @escaping (Tweak) -> Void) {
-        self.deregisterFromConfigurationsUpdates(object)
-        queue.sync {
-            let queue = OperationQueue.main
-            let name = TweakConfigurationDidChangeNotification
-            let notificationsCenter = NotificationCenter.default
-            let observer = notificationsCenter.addObserver(forName: name, object: nil, queue: queue) { notification in
-                guard let tweak = notification.userInfo?[TweakConfigurationDidChangeNotificationTweakKey] as? Tweak else { return }
-                closure(tweak)
-            }
-            observersMap[object] = observer
+        deregisterFromConfigurationsUpdates(object)
+        let queue = OperationQueue.main
+        let name = TweakConfigurationDidChangeNotification
+        let notificationsCenter = NotificationCenter.default
+        let observer = notificationsCenter.addObserver(forName: name, object: nil, queue: queue) { notification in
+            guard let tweak = notification.userInfo?[TweakConfigurationDidChangeNotificationTweakKey] as? Tweak else { return }
+            closure(tweak)
         }
+        _observersMap.mutate { $0[object] = observer }
     }
     
     public func deregisterFromConfigurationsUpdates(_ object: NSObject) {
-        queue.sync {
-            guard let observer = observersMap[object] else { return }
-            NotificationCenter.default.removeObserver(observer)
-            observersMap.removeValue(forKey: object)
-        }
+        guard let observer = observersMap[object] else { return }
+        NotificationCenter.default.removeObserver(observer)
+        _observersMap.mutate { $0.removeValue(forKey: object) }
     }
     
     @objc private func configurationDidChange() {
@@ -189,10 +176,8 @@ extension TweakManager {
 extension TweakManager {
     
     public func resetCache() {
-        queue.sync {
-            featureCache = [String : Bool]()
-            tweakCache = [String : [String : Tweak]]()
-            experimentCache = [String : String]()
-        }
+        _featureCache.mutate { $0.removeAll() }
+        _tweakCache.mutate { $0.removeAll() }
+        _experimentCache.mutate { $0.removeAll() }
     }
 }
