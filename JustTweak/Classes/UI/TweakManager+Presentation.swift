824c031e93f8ce24b1afd76c91ac95b033082769
Alberto De Bortoli <albertodebortoli@gmail.com>
2020-02-02 17:00:58 +0000

    Add support to correctly display tweaks from multiple local configurations

--- JustTweak/Classes/UI/TweakManager+Presentation.swift
+++ JustTweak/Classes/UI/TweakManager+Presentation.swift
@@ -8,28 +8,28 @@ import Foundation
 extension TweakManager {
     
     var displayableTweaks: [Tweak] {
-        guard let localConfiguration = self.localConfiguration else {
-            return []
-        }
-        var tweaks = [Tweak]()
-        for (feature, variables) in localConfiguration.features {
-            for variable in variables {
-                if let tweak = tweakWith(feature: feature, variable: variable) {
-                    let jsonTweak = localConfiguration.tweakWith(feature: feature, variable: variable)
-                    let aggregatedTweak = Tweak(feature: feature,
-                                                variable: variable,
-                                                value: tweak.value,
-                                                title: jsonTweak?.title,
-                                                description: jsonTweak?.desc,
-                                                group: jsonTweak?.group)
-                    tweaks.append(aggregatedTweak)
+        var tweaks = [String : Tweak]()
+        for localConfiguration in self.localConfigurations.reversed() {
+            for (feature, variables) in localConfiguration.features {
+                for variable in variables {
+                    if let tweak = tweakWith(feature: feature, variable: variable),
+                        let jsonTweak = localConfiguration.tweakWith(feature: feature, variable: variable) {
+                        let aggregatedTweak = Tweak(feature: feature,
+                                                    variable: variable,
+                                                    value: tweak.value,
+                                                    title: jsonTweak.title,
+                                                    description: jsonTweak.desc,
+                                                    group: jsonTweak.group)
+                        let key = "\(feature)-\(variable)"
+                        tweaks[key] = aggregatedTweak
+                    }
                 }
             }
         }
-        return tweaks.sorted(by: { $0.displayTitle < $1.displayTitle })
+        return tweaks.values.sorted(by: { $0.displayTitle < $1.displayTitle })
     }
     
-    private var localConfiguration: LocalConfiguration? {
-        return configurations.first { $0 is LocalConfiguration } as? LocalConfiguration
+    private var localConfigurations: [LocalConfiguration] {
+        return configurations.filter { $0 is LocalConfiguration } as! [LocalConfiguration]
     }
 }
