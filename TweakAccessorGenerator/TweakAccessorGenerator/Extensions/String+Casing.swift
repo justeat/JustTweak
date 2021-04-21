11b6d9648621a05abda1d98cac145eeb2e4e5d19
Alberto De Bortoli <alberto.debortoli@just-eat.com>
2021-04-21 14:41:14 +0100

    Improve camelCased implementation

--- TweakAccessorGenerator/TweakAccessorGenerator/Extensions/String+Casing.swift
+++ TweakAccessorGenerator/TweakAccessorGenerator/Extensions/String+Casing.swift
@@ -8,14 +8,18 @@ import Foundation
 extension String {
     
     func camelCased(with separator: Character = "_") -> String {
-        self.lowercased()
+        self
             .split(separator: separator)
             .enumerated()
-            .map { $0.offset > 0 ? $0.element.capitalized : $0.element.lowercased() }
+            .map { $0.offset == 0 ? String($0.element).lowercasedFirstChar() : String($0.element).capitalisedFirstChar() }
             .joined()
     }
     
-    func lowercaseFirstChar() -> String {
+    func lowercasedFirstChar() -> String {
         prefix(1).lowercased() + self.dropFirst()
+    }
+    
+    func capitalisedFirstChar() -> String {
+        prefix(1).uppercased() + self.dropFirst()
     }
 }
